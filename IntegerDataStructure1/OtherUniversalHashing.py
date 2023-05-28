2875f23e9a95671a89ab0703c4c17116dae057fe
Fenfen <59223215+katherine-design@users.noreply.github.com>
2023-05-28 15:00:25 +0200

    add other universal hashing

--- IntegerDataStructure1/OtherUniversalHashing.py
+++ IntegerDataStructure1/OtherUniversalHashing.py
@@ -6,70 +6,51 @@
 # Goal: a compact data structure with fast operations
 
 """
-data structure of the universal Hashing:
-(1) H is a family of functions mapping U to {0,...,m-1}.
-(2) For any x != y in U and h chosen uniformly at random in H, we have Pr(h(x) = h(y)) <= 1/m
-
-the definition of hash function defined as follow: (Universal hashing)
-Given a prime m and a = (a_1 a_2 a_3 ... a_r)_m, we define 
-TODO
+data structure of this universal Hashing:
+For prime p > 0, 1 < a < p-1; 0 < b < p - 1, we define: TODO
 
 """
 
 # Time: O(1) expected time per operation(lookup, insert, delete)
 # Space: O(n) n elements in the dictionary
-def base_m_representation(x, m):
-        if m < 2 or not isinstance(m, int):
-            raise ValueError("m must be a prime number greater than or equal to 2.")
-        if x == 0:
-            return [0]
-        result = []
-        while x > 0:
-            remainder = x % m
-            result.append(remainder)
-            x = x // m # floor division operator
-        return result[::-1] # return the result list in reverse order
+
+
+import random
 
 class UniversalHashing(object):
-    def __init__(self, bucket, a, m):
+    # p is a prime
+    def __init__(self, bucket, p):
         self.__bucket = bucket
         self.__table = [[] for _ in range(bucket)]
-        self.__a = a
-        self.__m = m
+        self.__p = p
 
-    # define hash function: h
-    """
-    m is a prime
-    we use base-m representation to rewrite x and a  (x written in base m; same for a)
-    """
-    def hashFunction(self, key, a, m):
-        aBaseM = base_m_representation(a, m)
-        keyBaseM = base_m_representation(key, m)
-        result = 0
-        for index1,index2 in zip(aBaseM,keyBaseM):
-            result += index1*index2
-        return result % m
+    # define other universal hashing function: h
+    def hashFunction(self, key, p):
+        a = random.randint(1, p-1)
+        b = random.randint(0, p-1)
+        index = (a*key + b)%p
+        return index
 
 
     # compute h(x); scan through list for h(x). Return true if x is in list and false otherwise
-    def LookupItem(self, key, a, m):
-        index = self.hashFunction(key, a, m)
+    def LookupItem(self, key, p):
+        index = self.hashFunction(key, p)
         if key not in self.__table[index]:
             return False
         else:
             return True
     
-    def InsertItem(self, key, a, m):
-        index = self.hashFunction(key, a, m)
+    def InsertItem(self, key, p):
+        index = self.hashFunction(key, p)
 
         # using append function, this element gonna be add at the end of the list
         # self.__table[index].append(key)
         # add this element in the front of this list
         self.__table[index].insert(0, key)
     
-    def DeleteItem(self, key, a, m):
+    def DeleteItem(self, key, p):
         # compute h(x)
-        index = self.hashFunction(key, a, m)
+        index = self.hashFunction(key, p)
 
         # scan through lish for h(x). If x is in list remove it. Otherwise, do nothing
         if key not in self.__table[index]:
@@ -90,18 +71,18 @@ if __name__ == "__main__":
     # a_before = [1, 16, 41, 54, 66, 96, 214]
     a_before = [1,11,21,31,41,51,61] 
     bucket_size = 10
-    a = 107
-    m = 7 # prime
-    h = UniversalHashing(bucket_size,a,m) 
+    # a = 107
+    p = 7 # prime
+    h = UniversalHashing(bucket_size,p) 
    
     print(a_before)
 
     # insert all elements
     for x in a_before:
-        h.InsertItem(x, a, m)
+        h.InsertItem(x, p)
 
     h.DisplayHash()
-    h.InsertItem(3, a, m)
+    h.InsertItem(3, p)
     h.DisplayHash()
     # h.InsertItem(4, a, m)
     # h.DisplayHash()
